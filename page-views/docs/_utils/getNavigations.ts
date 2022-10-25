19c888085ae74aac92ba86334cd0b5d63159cff0
Loi Le <tanloiit2010@gmail.com>
2022-10-25 12:20:35 +0700

    fix: add filter index.md file

--- page-views/docs/_utils/getNavigations.ts
+++ page-views/docs/_utils/getNavigations.ts
@@ -4,31 +4,34 @@ import fs from 'fs'
 import getNameFromSlug from './getNameFromSlug'
 
 const getNavigations: (pathName: string) => INavItem[] = (pathName: string) =>
-  fs.readdirSync(pathName).reduce((result: INavItem[], item: string) => {
-    const slug = item.replace(/\.md$/, '')
+  fs
+    .readdirSync(pathName)
+    .filter((item) => item !== 'index.md')
+    .reduce((result: INavItem[], item: string) => {
+      const slug = item.replace(/\.md$/, '')
 
-    if (item.endsWith('.md')) {
-      return [
-        ...result,
-        {
-          name: getNameFromSlug(slug),
-          slug,
-        },
-      ]
-    } else {
-      const isContainIndexFile = fs.existsSync(
-        path.join(path.join(pathName || '', item), 'index.md'),
-      )
+      if (item.endsWith('.md')) {
+        return [
+          ...result,
+          {
+            name: getNameFromSlug(slug),
+            slug,
+          },
+        ]
+      } else {
+        const isContainIndexFile = fs.existsSync(
+          path.join(path.join(pathName || '', item), 'index.md'),
+        )
 
-      return [
-        ...result,
-        {
-          name: getNameFromSlug(slug),
-          slug: isContainIndexFile ? slug : null,
-          children: getNavigations(path.join(pathName, item)),
-        },
-      ]
-    }
-  }, [])
+        return [
+          ...result,
+          {
+            name: getNameFromSlug(slug),
+            slug: isContainIndexFile ? slug : null,
+            children: getNavigations(path.join(pathName, item)),
+          },
+        ]
+      }
+    }, [])
 
 export default getNavigations
