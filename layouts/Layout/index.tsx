35aa9d84951de7eb5c3fe9dc8f9a6310f64bcfe5
Loi Le <tanloiit2010@gmail.com>
2022-10-29 16:25:01 +0700

    feat: add mailchimp integration

--- layouts/Layout/index.tsx
+++ layouts/Layout/index.tsx
@@ -1,5 +1,7 @@
 import { Modal } from 'components'
 import Head from 'next/head'
+import Script from 'next/script'
+
 import { useState } from 'react'
 import Header from './Header'
 import LayoutProps from './Layout.props'
@@ -19,6 +21,11 @@ const Layout: React.FunctionComponent<LayoutProps> = ({
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script id="mcjs" crossOrigin="anonymous" strategy="lazyOnload">
+        {`
+             !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/b00200417b3d893cd93cbedee/15c594d23cb2f90f16d5d899f.js");
+              `}
+      </Script>
       <div className="relative overflow-hidden">
         <Header onSignIn={onSignIn} onSignUp={onSignUp} />
         <main className="mt-[72px] md:mt-[74px]">{children}</main>
