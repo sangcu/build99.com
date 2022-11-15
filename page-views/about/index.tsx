8004f1c9b236dfd087d61a34ebbde9c35d3a2482
Loi Le <tanloiit2010@gmail.com>
2022-11-15 17:50:37 +0700

    fix: about page styling

--- page-views/about/index.tsx
+++ page-views/about/index.tsx
@@ -103,11 +103,11 @@ const About: NextPageWithLayout = () => {
             </span>
           </h2>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-            <div className="inline-flex rounded-md shadow">
+            <div className="inline-flex rounded-md">
               <Link href={CONTACT_PAGE_URL}>
                 <a
                   target="_blank"
-                  className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white py-3 px-5 text-base font-medium text-grape-600 shadow-md hover:bg-grape-50 sm:w-auto"
+                  className="shadow mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white py-3 px-5 text-base font-medium text-grape-600 shadow-md hover:bg-grape-50 sm:w-auto"
                 >
                   <span>{t('Contact Us')}</span>
                   <ArrowTopRightOnSquareIcon
