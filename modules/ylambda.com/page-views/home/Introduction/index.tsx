7dd47199dc04126ee4e2fa40bb1c57e43aabdd56
sang.cu <sangcn@gmail.com>
2023-03-01 14:29:53 +0700

    change CTA text

--- modules/ylambda.com/page-views/home/Introduction/index.tsx
+++ modules/ylambda.com/page-views/home/Introduction/index.tsx
@@ -21,7 +21,7 @@ const Introduction: React.FC = () => {
           </h1>
           <p className="mt-8 !h-12 flex-none inline-flex justify-center items-center rounded-md border border-transparent bg-orange-600 px-6 py-2 text-base font-medium text-white hover:bg-orange-700" >
             <Link href={'https://leader.ylambda.com'}>
-              TRY NOW. free for individual leader
+              Onboard your team now. Get free for individual leader.
             </Link>
           </p>
         </div>
