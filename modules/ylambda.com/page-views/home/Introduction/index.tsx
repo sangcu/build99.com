217d09dd7cfd9519f1192aa54c592c3458cc8761
sang.cu <sangcn@gmail.com>
2023-03-03 11:10:45 +0700

    landing: text

--- modules/ylambda.com/page-views/home/Introduction/index.tsx
+++ modules/ylambda.com/page-views/home/Introduction/index.tsx
@@ -19,11 +19,17 @@ const Introduction: React.FC = () => {
               {t('with automation toolkit.')}
             </span>
           </h1>
-          <p className="mt-8 !h-12 flex-none inline-flex justify-center items-center rounded-md border border-transparent bg-orange-600 px-6 py-2 text-base font-medium text-white hover:bg-orange-700" >
-            <Link href={'https://leader.ylambda.com'}>
-              Onboard your team now. Get free for individual leader.
-            </Link>
+
+          <p className="pt-8 text-xl leading-6 text-white">
+            yLambda provides a toolkit and suggestion framework based on
+            scientific method to help your leadership team plays the role with
+            minimal effort.
           </p>
+          <button className="mt-8 !h-12 flex-none inline-flex justify-center items-center rounded-md border border-transparent bg-orange-600 px-6 py-2 text-base font-medium text-white hover:bg-orange-700">
+            <Link href={'https://leader.ylambda.com'}>
+              ONBOARD YOUR TEAM NOW. It's free.
+            </Link>
+          </button>
         </div>
       </div>
     </div>
