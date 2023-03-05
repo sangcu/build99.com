bdbd4c3175e0c66109c6b842e781dd0c571388c1
sang.cu <sangcn@gmail.com>
2023-03-05 17:42:17 +0700

    feat: add ms clarity

--- modules/ylambda.com/pages/_app.tsx
+++ modules/ylambda.com/pages/_app.tsx
@@ -57,6 +57,15 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         })(window,document,'script','dataLayer','GTM-TF9QTDZ');
       `}
         </Script>
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "g3thda132s");
+      `}
+        </Script>
         <NextSeo
           title={t(title)}
           description={t(description)}
