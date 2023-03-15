23ab54dbe4b1d0cfa81f5182df13d3e1414ae46e
Loi Le <tanloiit2010@gmail.com>
2023-03-15 18:43:08 +0700

    feat: revert code

--- modules/teamx/src/app/(pages)/dashboard/layout/components/SideBar/index.tsx
+++ modules/teamx/src/app/(pages)/dashboard/layout/components/SideBar/index.tsx
@@ -1,28 +1,26 @@
 import Link from "next/link";
 import { NavigationItem } from "../..";
 import classNames from "classnames";
-import { useRouter } from "next/navigation";
 
 interface SideBarProps {
   navigation: NavigationItem[];
 }
 
 const SideBar: React.FC<SideBarProps> = ({ navigation }) => {
-  const router = useRouter();
   return (
     <div className="hidden min-w-0 lg:flex">
       <div className="ml-8 xl:w-64 xl:flex-shrink-0 xl:border-r xl:border-gray-200">
         <nav className="mt-6 pr-4">
           <div className="space-y-1">
             {navigation.map((item) => (
-              <div
-                onClick={() => router.push(item.href)}
+              <Link
+                href={item.href}
                 key={item.name}
                 className={classNames(
                   item.current
                     ? "bg-orange-600 text-white"
                     : "text-gray-700 hover:bg-orange-50 hover:text-gray-900",
-                  "cursor-pointer group flex items-center rounded-md px-2 py-2 text-sm font-medium"
+                  "group flex items-center rounded-md px-2 py-2 text-sm font-medium"
                 )}
               >
                 <item.icon
@@ -35,7 +33,7 @@ const SideBar: React.FC<SideBarProps> = ({ navigation }) => {
                   aria-hidden="true"
                 />
                 {item.name}
-              </div>
+              </Link>
             ))}
           </div>
         </nav>
