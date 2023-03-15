4415166639007b001c2937a6d390b6ccac7a0591
Loi Le <tanloiit2010@gmail.com>
2023-03-15 18:36:42 +0700

    fix: test link for reload issue

--- modules/teamx/src/app/(pages)/dashboard/layout/components/SideBar/index.tsx
+++ modules/teamx/src/app/(pages)/dashboard/layout/components/SideBar/index.tsx
@@ -1,39 +1,40 @@
 import Link from "next/link";
 import { NavigationItem } from "../..";
 import classNames from "classnames";
+import { useRouter } from "next/navigation";
 
 interface SideBarProps {
   navigation: NavigationItem[];
 }
 
 const SideBar: React.FC<SideBarProps> = ({ navigation }) => {
+  const router = useRouter();
   return (
     <div className="hidden min-w-0 lg:flex">
       <div className="ml-8 xl:w-64 xl:flex-shrink-0 xl:border-r xl:border-gray-200">
         <nav className="mt-6 pr-4">
           <div className="space-y-1">
             {navigation.map((item) => (
-              <div key={item.name}>
-                <Link
-                  href={item.href}
+              <div
+                onClick={() => router.push(item.href)}
+                key={item.name}
+                className={classNames(
+                  item.current
+                    ? "bg-orange-600 text-white"
+                    : "text-gray-700 hover:bg-orange-50 hover:text-gray-900",
+                  "cursor-pointer group flex items-center rounded-md px-2 py-2 text-sm font-medium"
+                )}
+              >
+                <item.icon
                   className={classNames(
                     item.current
-                      ? "bg-orange-600 text-white"
-                      : "text-gray-700 hover:bg-orange-50 hover:text-gray-900",
-                    "group flex items-center rounded-md px-2 py-2 text-sm font-medium"
+                      ? "text-white-500"
+                      : "text-gray-400 group-hover:text-gray-500",
+                    "mr-3 h-6 w-6 flex-shrink-0"
                   )}
-                >
-                  <item.icon
-                    className={classNames(
-                      item.current
-                        ? "text-white-500"
-                        : "text-gray-400 group-hover:text-gray-500",
-                      "mr-3 h-6 w-6 flex-shrink-0"
-                    )}
-                    aria-hidden="true"
-                  />
-                  {item.name}
-                </Link>
+                  aria-hidden="true"
+                />
+                {item.name}
               </div>
             ))}
           </div>
